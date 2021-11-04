fbf48514cb8cb7b715a4365842fd26d5ff96dfb8
mrkvon <mrkvon@protonmail.com>
2021-11-04 23:01:38 +0100

    Improve and style Login

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -11,18 +11,56 @@ const Login = () => {
     dispatch(login(provider))
   }
   return (
-    <div>
-      <h1>Sign in with your Solid Identity</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="provider">Select your Solid Pod Provider</label>
-        <input
-          id="provider"
-          type="text"
-          value={provider}
-          onChange={e => setProvider(e.target.value)}
-        />
-        <input type="submit" value="Sign In" />
-      </form>
+    <div className="flex flex-col items-center justify-evenly min-h-screen m-5">
+      <div className="space-y-10">
+        <section className="flex flex-col items-center space-y-2">
+          <header>
+            <h1 className="text-2xl font-bold">Open Hospitality</h1>
+            <p className="italic">hospitality, decentralized</p>
+          </header>
+          <div className="w-80 flex items-center justify-evenly">
+            <a href="https://openhospitality.network">
+              <img
+                src="https://openhospitality.network/assets/img/logo.png"
+                title="Open Hospitality Network"
+                alt="Open Hospitality Network logo"
+                className="h-32"
+              />
+            </a>
+            <span className="text-3xl">+</span>
+            <a href="https://solidproject.org">
+              <img
+                src="https://avatars3.githubusercontent.com/u/14262490?v=3&s=200"
+                title="Solid Project"
+                alt="Solid Project logo"
+                className="h-32 p-5"
+              />
+            </a>
+          </div>
+        </section>
+        <form
+          className="w-96 flex flex-col items-center space-y-2"
+          onSubmit={handleSubmit}
+        >
+          <header className="text-xl font-bold">
+            Sign in with your Solid Identity
+          </header>
+          <label htmlFor="provider">Select your Solid Pod Provider</label>
+          <input
+            className="rounded border px-2 py-1 w-full"
+            id="provider"
+            type="text"
+            value={provider}
+            onChange={e => setProvider(e.target.value)}
+          />
+          <button
+            className="bg-blue-900 text-blue-50 rounded px-4 py-1 w-full"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
