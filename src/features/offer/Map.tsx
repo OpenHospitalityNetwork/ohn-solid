8dd89007a59efbc9bbdaac1239819112080372fa
mrkvon <mrkvon@protonmail.com>
2021-11-05 15:47:44 +0100

    Change initial map view

--- src/features/offer/Map.tsx
+++ src/features/offer/Map.tsx
@@ -1,20 +1,31 @@
-import React, { FC } from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { LatLngTuple } from 'leaflet'
-import MarkerClusterGroup from 'react-leaflet-markercluster'
-import { Offer } from './types'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import React, { FC } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
+import { Offer } from './types'
+
+/* uncomment CurrentLocation to go to user's current location
+const CurrentLocation = () => {
+  const map = useMap()
+  useEffect(() => {
+    map.locate()
+  }, [map])
+  useMapEvents({
+    locationfound: e => map.setView(e.latlng, 4),
+  })
+
+  return null
+}
+*/
 
 const Map: FC<{ points: Offer[] }> = ({ points }) => {
-  const position: LatLngTuple = [50, 15]
+  const position: LatLngTuple = [30, 0]
 
   return (
-    <MapContainer
-      center={position}
-      zoom={10}
-      style={{ width: '100vw', height: '100vh' }}
-    >
+    <MapContainer center={position} zoom={2} className="w-screen h-screen">
+      {/*<CurrentLocation />*/}
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
