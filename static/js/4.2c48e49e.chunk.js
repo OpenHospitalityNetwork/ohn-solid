2c2885cf441f45d678660a294834689c673dbf18
mrkvon <mrkvon@users.noreply.github.com>
2021-11-05 04:49:01 +0000

    deploy: cdd3b3b4fc10e4378a0b03de9d1b8edb190d5807

---
+++ static/js/4.2c48e49e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpohn-solid-client"]=this["webpackJsonpohn-solid-client"]||[]).push([[4],{228:function(n,i){},230:function(n,i){}}]);
+//# sourceMappingURL=4.2c48e49e.chunk.js.map
--- static/js/4.b0404be5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpohn-solid-client"]=this["webpackJsonpohn-solid-client"]||[]).push([[4],{227:function(n,i){},229:function(n,i){}}]);
-//# sourceMappingURL=4.b0404be5.chunk.js.map
